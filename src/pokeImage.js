d88381c81710db357b87691bcb0fe6540cdf75d1
Catalina <c.otarola.arriagada@gmail.com>
2020-12-09 22:53:38 -0800

    updating pokeImage component

--- src/pokeImage.js
+++ src/pokeImage.js
@@ -12,27 +12,14 @@ function getRandomPokemonId(min, max) {
 
 //getting an id to start with
 const startPokemon = getRandomPokemonId(1, 887);
-
-//function for calling the API
-function getPokemonName() {
-  console.log("sending request to the pokeapi");
-  fetch(`${pokePath}${startPokemon}`)
-  .then((response) => {
-    if(response.status != 200) {
-      throw response.statusText;
-      console.log("*** Request not ok: " + response.statusText);
-    }
-    else {
-      response.json();
-    }
-  })
-  .then((data) => {
-    console.log(data);
-    //create a variable that stores the name of the pokemon
-    let name = data.name;
-    console.log(name);
-  })
+const startPokemon2 = getRandomPokemonId(1, 887);
+//while loop for getting a new id that's not the same as the one we already have
+while (startPokemon == startPokemon2) {
+  startPokemon2 = getRandomPokemonId(1, 877);
 }
+
+console.log(startPokemon);
+console.log(startPokemon2);
 
 //pokemon image and buttons  component
 class PokeImage extends React.Component {
@@ -40,15 +27,70 @@ class PokeImage extends React.Component {
     super(props);
     this.state = {
       pokeId: startPokemon,
+      pokeId2: startPokemon2,
       pokeImage: startPokemon,
       pokeName: "",
+      pokeName2: ""
     }
   }//constructor
 
+  //function for calling the API
+  getPokemonNames() {
+    //making request for first pokemon name
+    console.log("sending request to the pokeapi");
+    fetch(`${pokePath}${startPokemon}`)
+    .then((response) => {
+      if(response.status != 200) {
+        throw response.statusText;
+        console.log("*** Request not ok: " + response.statusText);
+      }
+      else {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      //create a variable that stores the name of the pokemon
+      let name1 = data.name;
+      console.log(name1);
+      //setting the state of the Component
+      this.setState({
+        pokeName: name1
+      });
+    })
+
+    //making request for second pokemon name
+    fetch(`${pokePath}${startPokemon2}`)
+    .then((response) => {
+      if(response.status != 200) {
+        throw response.statusText;
+        console.log("*** Request not ok: " + response.statusText);
+      }
+      else {
+        return response.json();
+      }
+    })
+    .then((data) => {
+      //create a variable that stores the name of the pokemon
+      let name2 = data.name;
+      console.log(name2);
+      //setting the state of the Component
+      this.setState({
+        pokeName2: name2
+      });
+    })
+  }
+
   render() {
+
     return(
-      <div className="Image">
+      <div>
+      <div className="Image" onload={this.getPokemonNames()}>
         <img src={`${imagePath}${this.state.pokeId}.png`} alt="pokemon-image" width="400" height="400" />
+      </div>
+      <div clasName="PokeBotones">
+        <button class="button1">{this.state.pokeName}</button>
+        <button class="button2">{this.state.pokeName2}</button>
+      </div>
       </div>
     );
   }
