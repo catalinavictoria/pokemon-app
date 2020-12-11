77d41b736cca71bcc59e025fd2163ace42f8fb90
Catalina <c.otarola.arriagada@gmail.com>
2020-12-10 17:10:10 -0800

    adding neccesary logic for setting pokemon names and buttons

--- src/pokeImage.js
+++ src/pokeImage.js
@@ -5,33 +5,25 @@ const imagePath = "https://pokeres.bastionbot.org/images/pokemon/";
 const pokePath = "https://pokeapi.co/api/v2/pokemon/";
 
 //this function returns a random number within a range
-function getRandomPokemonId(min, max) {
+function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
-//getting an id to start with
-const startPokemon = getRandomPokemonId(1, 887);
-const startPokemon2 = getRandomPokemonId(1, 887);
-//while loop for getting a new id that's not the same as the one we already have
-while (startPokemon == startPokemon2) {
-  startPokemon2 = getRandomPokemonId(1, 877);
-}
-
-console.log(startPokemon);
-console.log(startPokemon2);
 
 //pokemon image and buttons  component
 class PokeImage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pokeId: startPokemon,
-      pokeId2: startPokemon2,
-      pokeImage: startPokemon,
-      pokeName: "",
-      pokeName2: ""
+      pokeId: "",
+      pokeId2: "",
+      correctPokeId: "",
+      correctPokeName: "",
+      pokeImageId: "",
+      pokeName1: "",
+      pokeName2: "",
+
     }
   }//constructor
 
@@ -40,50 +32,98 @@ class PokeImage extends React.Component {
     this.getPokemonNames();
   }
 
-  //function for calling the API
+  //function for getting random ids and making request to api
   getPokemonNames() {
+    //variables
+    let correctName;
+    let correctId;
+    let name1;
+    let name2;
+    // generate two random ids
+    let p1 = getRandomInt(1, 887);
+    let p2 = getRandomInt(1, 887);
+    //while loop for getting a new id that's not the same as the one we already have
+    while (p1 === p2) {
+      p2 = getRandomInt(1, 887);
+    }
+
+    //setting correct pokemon id
+    if (getRandomInt(1, 2) === 1) {
+      correctId = p1;
+    }
+    else {
+      correctId = p2;
+    }
+
+    //changing the state of the component
+    this.setState({
+      pokeId: p1,
+      pokeId2: p2,
+      correctPokeId: correctId
+    });
+
     //making request for first pokemon name
     console.log("sending request to the pokeapi");
-    fetch(`${pokePath}${startPokemon}`)
+    fetch(`${pokePath}${p1}`)
     .then((response) => {
-      if(response.status != 200) {
-        throw response.statusText;
+      if(response.status !== 200) {
         console.log("*** Request not ok: " + response.statusText);
+        throw response.statusText;
       }
       else {
         return response.json();
       }
     })
     .then((data) => {
-      //create a variable that stores the name of the pokemon
-      let name1 = data.name;
+      //variable that stores the name of the pokemon
+      name1 = data.name;
       console.log(name1);
-      //setting the state of the Component
-      this.setState({
-        pokeName: name1
-      });
-    })
+      //making request for second pokemon name
+      fetch(`${pokePath}${p2}`)
+      .then((response) => {
+        if(response.status !== 200) {
+          console.log("*** Request not ok: " + response.statusText);
+          throw response.statusText;
+        }
+        else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        //variable that stores the name of the pokemon
+        name2 = data.name;
+        console.log(name2);
+        //if statement for setting state of correct pokemon name
+        if (p1 === correctId) {
+          correctName = name1;
+        }
+        else {
+          correctName = name2;
+        }
+        //checking everything
+        console.log("correct id: " + correctId);
+        console.log(p1);
+        console.log(p2);
+        console.log(name1);
+        console.log(name2);
+        console.log(correctName);
 
-    //making request for second pokemon name
-    fetch(`${pokePath}${startPokemon2}`)
-    .then((response) => {
-      if(response.status != 200) {
-        throw response.statusText;
-        console.log("*** Request not ok: " + response.statusText);
-      }
-      else {
-        return response.json();
-      }
+        //setting the state of the Component
+        this.setState({
+          pokeName1: name1,
+          pokeName2: name2,
+          correctPokeName: correctName
+        });
+      })
     })
-    .then((data) => {
-      //create a variable that stores the name of the pokemon
-      let name2 = data.name;
-      console.log(name2);
-      //setting the state of the Component
-      this.setState({
-        pokeName2: name2
-      });
-    })
+    //
+    // //setting the state
+    // this.setState({
+    //   correctPokeName: correctName
+    // });
+    //checking everything
+
+
   }
 
   render() {
@@ -91,10 +131,10 @@ class PokeImage extends React.Component {
       <div className="poke-image">
         <h1 className="titulo">Guess The Pokemon!</h1>
         <div className="image">
-          <img className="the-image" src={`${imagePath}${this.state.pokeId}.png`} alt="pokemon-image" width="400" height="400" />
+          <img className="the-image" src={`${imagePath}${this.state.pokeId}.png`} alt="pokemon" width="400" height="400" />
         </div>
         <div className="poke-botones">
-          <button className="button1">{this.state.pokeName}</button>
+          <button className="button1">{this.state.pokeName1}</button>
           <button className="button2">{this.state.pokeName2}</button>
         </div>
       </div>
